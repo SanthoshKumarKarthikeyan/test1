fe7d823a58ce12c878c8ba55b88c2848ee26b4d9
Santhosh Kumar Karthikeyan <46432913+SanthoshKumarKarthikeyan@users.noreply.github.com>
2019-02-08 12:00:57 -0500

    Create Assignment5.r

---
+++ Assignment5.r
@@ -0,0 +1,14 @@
+A <- matrix(1:100, nrow=10) 
+B <- matrix(1:1000, nrow=10) 
+t(A)
+t(B)
+a <- c(1:2)
+b <- c(1:4)
+X <- a*A
+Y <- b*B
+a <- c(1:10)
+b <- c(1:100)
+A%*%a
+B%*%b
+S <- matrix(2:5,nrow=2)
+det(s)
