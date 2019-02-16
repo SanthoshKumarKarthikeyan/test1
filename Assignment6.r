b0b3ddf3a514181911ba3d4906e01927733b0f82
Santhosh Kumar Karthikeyan <46432913+SanthoshKumarKarthikeyan@users.noreply.github.com>
2019-02-15 21:06:21 -0500

    Create Assignment6.r

---
+++ Assignment6.r
@@ -0,0 +1,14 @@
+#Answer for 1
+A=matrix(c(2,0,1,3),ncol=2)
+B=matrix(c(5,2,4,-1),ncol=2)
+A+B
+A-B
+
+#Answer for 2
+C <- diag(c(4,1,2,3))
+C
+
+#Answer for 3
+D <- matrix(c(3,2,2,2,2,1,3,0,0,0,1,0,3,0,0,1,0,0,3,0,1,0,0,0,3),nrow=5)
+D
+
