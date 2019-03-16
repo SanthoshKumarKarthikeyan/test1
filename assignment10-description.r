4bee9d6631bf2469d5a2ed2f49918f4928c1e998
Santhosh Kumar Karthikeyan <46432913+SanthoshKumarKarthikeyan@users.noreply.github.com>
2019-03-16 02:11:21 -0400

    Create assignment10-description.r

---
+++ assignment10-description.r
@@ -0,0 +1,13 @@
+Package: skkr
+Type: Package
+Title: Performing statistical analysis
+Version: 0.1.0
+Author: Santhosh Kumar Karthikeyan
+Maintainer: Santhosh Kumar Karthikeyan <santhoshkum2@mail.usf.edu>
+Description: Making a single package that has tools to do statistical analysis 
+             like T-test, z-score normalisation, PCA , and to visualize them with 
+             graph and plots such as box plot, heat maps, dendograms.
+Depends: R (>= 3.1.2)
+License: CC0
+Encoding: UTF-8
+LazyData: true
